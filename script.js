4486e7f383140703962228ac2b233c80c455bb75
dejo55 <dtotorino@gmail.com>
2021-09-28 23:21:56 +0000

    New upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const colors= ['red','green','blue','rgba{100,23,120,200}','#ffaadd'];
+
+const btn= document.getElementById('btn');
+const color= document.querySelector('.color');
+
+
+btn.addEventListener('click', function(){
+
+
+// console.log( colors.length);
+
+   
+    const randomNumber=getRamdom();
+
+   document.body.style.backgroundColor= colors[randomNumber];
+
+   color.textContent= colors[randomNumber];
+
+});
+
+function getRamdom(){
+ 
+
+    return Math.floor( Math.random()*colors.length);
+}
