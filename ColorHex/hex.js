9817b0977bc4cfc4b1963013e1f67dd2c4fa7e9b
dejo55 <dtotorino@gmail.com>
2021-09-29 00:17:32 +0000

    Updat e (12:18)

---
+++ ColorHex/hex.js
@@ -0,0 +1,32 @@
+const hex=[ 0,1,2,3,4,5,6,7,8,9,'A','B', 'C' , "D" , 'E' , 'F' ];
+
+
+
+const btn = document.getElementById('btn');
+const color= document.querySelector('.color');
+
+btn.addEventListener('click',function(){
+
+    let hexNum= '#';
+
+    for( let i=0; i<6; i++){
+        hexNum += hex[getRam()];
+
+     
+    };
+  
+document.body.style.backgroundColor=hexNum;
+
+color.textContent=hexNum;
+
+
+});
+
+
+
+
+
+function getRam(){
+
+    return Math.floor(Math.random() * hex.length)
+};
